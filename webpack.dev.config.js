8ff07586962c4daa8a2c0faad33ea83bc6f048ea
hooman mousavi <hoomanmousavi77@gmail.com>
2022-11-26 14:38:32 +0330

    refactoreing project wirh vue and bootstrap-vue

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,89 +2,66 @@ const path=require('path')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const { VueLoaderPlugin } = require('vue-loader')
 module.exports={
-    entry: {
-        "main":"./src/javascript/popup.js",
-        "component":"./src/javascript/component.js",
-        "background":"./src/javascript/background.js"
-    },
+    entry: "./src/javascript/main.js",
     output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname,'./dist')
+        path: path.resolve(__dirname,'./dist'),
+        filename: "[name].[contenthash].js",
+        assetModuleFilename: "assets/[name][ext]"
     },
     mode: "development",
     module: {
         rules: [
-
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
             {
                 test: /\.css$/i,
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader",
+                ],
             },
             {
-                test: /\.(png|svg|jpe?g|json)/,
-                type: 'asset/resource',
-                generator: {
-                    filename: "assets/[name][ext]"
-                }
+                test: /\.(png|svg|jpe?g)/,
+                type: 'asset/resource'
             },
             {
-                test: /\.json/,
-                type: 'asset/resource',
-                generator: {
-                    filename: './[name][ext]'
-                }
+                test: /\.(eot|ttf|woff|woff2)/,
+                type: 'asset/inline'
             },
             {
                 test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
+                exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: "babel-loader"
                 }
-            },
-            {
-                test: /\.(eot|woff|woff2|ttf)$/,
-                type: 'asset/inline'
             }
-        ]
+        ],
     },
-    plugins: [new MiniCssExtractPlugin({
-        filename:'css/[name].css',
-    }),
+    plugins: [
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename:"css/[name].css"
+        }),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: "popup.html",
-            filename: "popup.html",
-            chunks: ["main","component"]
-        }),
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-        }),
+            template: "index.html"
+        })
     ],
     devServer: {
         static: {
-            directory: path.join(__dirname, "./dist"),
+            directory: path.join(__dirname, './dist'),
         },
         compress: true,
         port: 9000,
         devMiddleware:{
-            writeToDisk: true
+            writeToDisk:true
         }
-    },
-    resolve: {
-        fallback: {
-            util: require.resolve("util/"),
-            assert:require.resolve("assert/"),
-            path:require.resolve("path-browserify"),
-            url:require.resolve("url/"),
-            "fs": false
-        },
-        alias: {
-            process: "process/browser"
-        }
+
     }
 }
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,64 +3,60 @@ const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader')
 module.exports={
-    entry: {
-        "main":"./src/javascript/popup.js",
-        "component":"./src/javascript/component.js",
-        "background":"./src/javascript/background.js"
-    },
+    entry: "./src/javascript/main.js",
     output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname,'./dist'),
+        path: path.resolve(__dirname,"./dist"),
+        filename: "[name].[contenthash].js",
+        assetModuleFilename: "assets/[name][ext]"
     },
     mode: "production",
     module: {
         rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
             {
                 test: /\.css$/i,
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader",
+                ],
             },
             {
-                test: /\.(png|svg|jpe?g|json)/,
-                type: 'asset/resource',
-                generator: {
-                    filename: "assets/[name][ext]"
-                }
+                test: /\.(png|svg|jpe?g)/,
+                type: 'asset/resource'
             },
             {
-                test: /\.json/,
-                type: 'asset/resource',
-                generator: {
-                    filename: './[name][ext]'
-                }
+                test: /\.(eot|ttf|woff|woff2)/,
+                type: 'asset/inline'
             },
             {
                 test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
+                exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: "babel-loader"
                 }
-            },
-            {
-                test: /\.(eot|woff|woff2|ttf)$/,
-                type: 'asset/inline'
             }
-        ]
+        ],
     },
-    plugins: [new MiniCssExtractPlugin({
-        filename:'css/[name].css',
-    }),
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename:"css/[name].css"
+        }),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: "popup.html",
-            filename: "popup.html",
-            chunks: ["main","component"]
+            template: "index.html"
             // publicPath:"./dist/"
-        }),
-        new TerserPlugin()
+        })
     ]
 }
